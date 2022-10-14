869850d537fbded20d07540b6d1a31fa346ad9c2
eclipse2101 <rdericieux25@aoiths.org>
2022-10-14 09:01:42 -0400

    finished part 2 and 3

--- Assets/Scripts/RubuController.cs
+++ Assets/Scripts/RubuController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RubuController : MonoBehaviour
 {
 
     // Start is called before the first frame update
@@ -15,7 +15,15 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
        float horizontal = Input.GetAxis("Horizontal"); //this will call on the input that is under the name horizontal
-       Debug.Log(horizontal)
+       Debug.Log(horizontal);
+
+       float vertical = Input.GetAxis("Vertical"); 
+       Debug.Log(vertical);
+
+       Vector2 position = transform.position;
+       position.x = position.x + 5.0f * horizontal * Time.deltaTime; // this function will change the speed of how fast your character moves
+       position.y = position.y + 5.0f * vertical * Time.deltaTime;
+       transform.position = position;
 
     }
 }
