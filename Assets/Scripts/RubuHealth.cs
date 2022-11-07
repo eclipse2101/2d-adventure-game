c4073f2c274c948e3a03d49c8d36dc9b1c52bd64
eclipse2101 <rdericieux25@aoiths.org>
2022-11-07 09:12:20 -0500

    about to finish part 7

---
+++ Assets/Scripts/RubuHealth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubuHealth : MonoBehaviour
+{
+    public int Maxhealth = 3;
+    int currenthealth; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      currenthealth = Maxhealth;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangedHealth(int amount)
+    {
+        currenthealth = Mathf.Clamp(currenthealth + amount, 0, Maxhealth)
+        
+    }
+}
